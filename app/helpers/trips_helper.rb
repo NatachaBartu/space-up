78496d39584cf59b263b48399f8b60b22133f5b6
natacha <nat_bartu@hotmail.com>
2021-08-04 11:54:22 +1000

    add default imageand logic

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,2 +1,7 @@
 module TripsHelper
+def image_select(trip)
+        return trip.picture if trip.picture.attached?
+        #if no picture attached use default image
+        return "default-image.jpg"
+    end
 end
