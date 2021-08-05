c9b4a0c80f4b8a68ecb97316343e933b82ed132b
natacha <nat_bartu@hotmail.com>
2021-08-06 09:34:17 +1000

    reset database on heroku

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,14 +4,10 @@ class Trip < ApplicationRecord
   belongs_to :cabin
   accepts_nested_attributes_for :cabin
   has_one_attached :picture
-
-
   before_validation :convert_price_to_cents, if: :price_changed?
 
 
   private
-
-  
 
   def convert_price_to_cents 
     self.price = (self.attributes_before_type_cast["price"].to_f * 100).round
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-#create a trip
+#list the array planets (dropdown)
 planets = [
     {name: "Mercury"},
     {name: "Venus"},
@@ -27,8 +21,6 @@ user = User.find_by username: "Tester"
 if Profile.count == 0
     Profile.create(user: user, first_name:"First_name", last_name:"Last_name")
 end
-
-
 
 cabins = [ "Gold", "Silver", "Bronze" ]
 
