323e7870c5cb3161a746d08bd68cbce806855a80
natacha <nat_bartu@hotmail.com>
2021-08-07 15:08:48 +1000

    change on profile spec and delete style home page

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -8,12 +8,5 @@ class User < ApplicationRecord
   has_many :trips
 
   has_many :sold_orders, class_name: "Order", foreign_key: "seller_id"
-  has_many :bought_orders, class_name: "Order", foreign_key: "buyer_id"
-
-  
-  #validates :first_name, presence: true
-  #validates :last_name, presence: true
-  #validates :email, presence: true
-        
-        
+  has_many :bought_orders, class_name: "Order", foreign_key: "buyer_id"        
 end
--- src/spec/models/profile_spec.rb
+++ src/spec/models/profile_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Profile, type: :profile do
+
+    it 'ensures first name presence' do
+      user = User.new(last_name: 'Last', email: 'test@test.com').save
+      expect(user).to eq(false)
+    end
+
+    it 'ensures last name presence' do
+      user = User.new(first_name: 'First', email: 'test@test.com').save
+      expect(user).to eq(false)
+    end
+
+    it 'ensures email name presence' do
+      user = User.new(first_name: 'First', last_name: 'Last').save
+      expect(user).to eq(false)
+    end
+
+    it 'should save sucessfully' do
+      user = User.new(first_name: 'First', last_name: 'Last', email: 'test@test.com').save
+      expect(user).to eq(true)
+    end
+
+  end
+
 end
